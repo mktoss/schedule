e76e42dce5b1d2d7cd4870e954510f2b510cdf0c
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-03-27 15:16:06 +0900

    Add user model

--- db/migrate/20190325082752_devise_create_users.rb
+++ db/migrate/20190325082752_devise_create_users.rb
@@ -3,6 +3,8 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :name,               null: false, unique: true
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -32,10 +34,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps null: false
     end
 
+    add_index :users, :name,                 unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
