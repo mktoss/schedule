30bfbba4b024955f91388b055e9ccefe49692ece
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-03-28 16:07:34 +0900

    Modify project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   has_many :events
-  has_many :project_users
+  has_many :project_users, dependent: :destroy
   has_many :users, through: :project_users
 
   validates :name, presence: true
