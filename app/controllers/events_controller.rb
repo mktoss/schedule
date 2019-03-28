b20de1489e5062ec9c9a17b7839bafd35c3e2637
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-03-28 16:09:26 +0900

    Edit events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,14 @@
 class EventsController < ApplicationController
+  before_action :set_project
+
   def index
+    @event = Event.new
+    @events = @project.events
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
   end
 end
