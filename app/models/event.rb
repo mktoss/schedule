f0ed10922b0430129f85a080fcea398be88fc68f
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-04-01 17:17:28 +0900

    Add scope to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,9 @@ class Event < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 12 }
 
+  scope :get_todo, -> { where(all_day: "true").where(todo: "false") }
+  scope :get_executed, -> { where(all_day: "true").where(todo: "true") }
+
   enum color: [
     "#ff7f7f",
     "#ff7fbf",
