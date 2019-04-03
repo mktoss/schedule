ab2b4083975e9960e4bac2bc3d4f8720a2f7a3b3
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-04-03 12:40:16 +0900

    Rename get_user to search_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   def index
     @project = Project.find_by_id(params[:project_id])
     if @project
-      @users = User.remove_member(@project.users.ids).get_user(params[:keyword])
+      @users = User.remove_member(@project.users.ids).search_user(params[:keyword])
     else
-      @users = User.remove_current_user(current_user.id).get_user(params[:keyword])
+      @users = User.remove_current_user(current_user.id).search_user(params[:keyword])
     end
     respond_to do |format|
       format.json
